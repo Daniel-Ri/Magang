28584964f12afb765bb5e402af4f284af7da18c6
Bizza Shafwah <bizza.utsula@gmail.com>
2020-02-23 12:17:40 +0700

    Update PrintHaiNTimes 2

--- bebas.c
+++ bebas.c
@@ -8,8 +8,8 @@
 void PrintHaiNTimes (int N) {
     /* Implementasikan di sini */
     /* KAMUS LOKAL */
-    int i; // untuk looping
-    i ++ // untuk menaikkan nilai variabel i sebanyak 1 angka
+    // int i; (untuk looping)
+    // i ++ (untuk menaikkan nilai variabel i sebanyak 1 angka)
     /* ALGORITMA */
 	  int i;
 		  for (i = 1; i<=N; i++) {
