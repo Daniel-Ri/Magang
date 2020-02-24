34c7be3262994d63fca420aa50bcd3497c15a892
ilhamjangjayaC <60304550+ilhamjangjayaC@users.noreply.github.com>
2020-02-24 13:05:27 +0700

    Rollback berkas bebas.h

--- bebas.h
+++ bebas.h
@@ -1,62 +1,39 @@
-/* File      : bebas.h */
++/* File      : bebas.h */
 /* Tanggal   : 21 Februari 2020 */
 /* Topik     : Tugas cakru programming pandago pertama */
 
-#include <stdio.h>
-#include <iostream>
-#include <string.h>
-#include <sstream>
 #ifndef BEBAS_H
 #define BEBAS_H
 
 #include "boolean.h"
 
-string ke_str(int N){
-    string z;
-    z.push_back((char)(N+'0'));
-    return z;
-}
-
-
-void PrintHaiNTimes (int N){
+void PrintHaiNTimes (int N);
 /* Menulis "Hai" diakhiri newline sebanyak N kali */
-	int i = 0;
-	while(i<N){
-		printf("Hai\n");
-		i += 1;
-	}
-}
+/* Contoh : N = 3 */
+/* Output :
+   Hai
+   Hai
+   Hai                                             */
 
-
-void GanjilOrGenap (int X){
+void GanjilOrGenap (int X); 
 /* Jika X adalah bilangan ganjil maka muncul pesan
-*/
-/*    std::string Xstr = to_string(X);
-*/
-    char Xstr[100];
-    sprintf(Xstr, "%d", X);
-	if(X%2==0){
-		printf(" %s adalah bilangan genap\n", Xstr);
-    }
-	else{
-		printf(" %s adalah bilangan ganjil\n", Xstr);	}
-}
-
+   "X (dalam angka) adalah bilangan ganjil"        
+   dan sebaliknya (kedua-duanya diakhiri newline)  */
+/* Contoh : X = 3
+   Output :
+   3 adalah bilangan ganjil. */
 
 int DeretArit50 ();
 /* Menghasilkan nilai dari 
    1 + 2 + 3 + ... + 50     */
 /* Harus dikerjakan menggunakan sistem looping (for atau while) */
 
-void ModNumber (int X, int Y){
+void ModNumber (int X, int Y);
 /* Mengirimkan hasil dari X mod Y ke layar (diakhiri newline) */
 /* Contoh : X = 10; Y = 3
    Output : 
    Hasil dari 10 mod 3 adalah 1 */
-   int m = X % Y;
-   cout<<"Hasil dari " +ke_str(X)+ " mod " +ke_str(Y)+ " adalah " ke_str(m)<<endl;
 
-}
 int TambahOrKurang (int A, int B, int C);
 /* Jika A adalah ganjil maka menghasilkan nilai dari
    pertambahan B dan C */
